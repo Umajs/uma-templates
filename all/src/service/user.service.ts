7d7c3e9e84e280e0bc942cd7ddd777bd28d44aea
dazjean <zunyi_zjj@163.com>
2022-03-07 14:20:33 +0800

    fix: the sample code used by inject in the template

--- all/src/service/user.service.ts
+++ all/src/service/user.service.ts
@@ -2,7 +2,7 @@ import { Inject, BaseService } from '@umajs/core';
 import User from '../model/User';
 
 export default class extends BaseService {
-    @Inject('User')
+    @Inject(User)
     user: User;
 
     getDefaultUserAge() {
